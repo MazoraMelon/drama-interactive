98abe0ed67744501b1e5ac50757007307b38c14b
Mazora <64915756+MazoraMelon@users.noreply.github.com>
2024-04-25 18:43:34 +0100

    ASKDJHKAJSDH

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,11 @@ import { useState, useEffect } from 'react';
 import './App.css';
 import Message from './Components/Message';
 import Sender from './Components/Sender';
+import { Route, Routes } from 'react-router-dom';
 
 import { createClient } from '@supabase/supabase-js';
+import Chat from './pages/Chat.jsx';
+
 
 const supabaseUrl = "https://jftaxymlbutkjoacvtbk.supabase.co";
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpmdGF4eW1sYnV0a2pvYWN2dGJrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTM5NjkxOTMsImV4cCI6MjAyOTU0NTE5M30.2aT1UzmiN34aKLNRUXAKwhKfnuwxMoiM3eMkHs1oyZU";
@@ -52,6 +55,12 @@ function App() {
 
   return (
     <>
+
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/chat" element={<Chat />} />
+      </Routes>
+
       <h1 style={{
         color: 'grey',
         fontSize: '1.5rem',
---
+++ src/pages/Chat.jsx
@@ -0,0 +1,14 @@
+import "../App.css";
+import { Route, Routes } from "react-router-dom";
+
+function Chat() {
+    alert("HI!!");
+    return (
+        <>
+            <h1>HI!!</h1>
+        </>
+    )
+
+}
+
+export default Chat;
